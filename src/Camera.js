2f1f6b3ae5ba95d9cf746bfe041b3bb6e45d6258
frenkield <david.frenkiel@yahoo.com>
2022-03-19 18:47:15 -1000

    Changed markup to use React refs

--- src/Camera.js
+++ src/Camera.js
@@ -2,69 +2,61 @@ import React from "react"
 
 export default class Camera extends React.Component {
 
-
-  // navigator.mediaDevices.getUserMedia({video: true})
-  // .then(function(stream) {
-  //   console.log(stream)
-
-  //   camera = document.getElementById("camera");
-
-  //   camera.srcObject = stream;
-
-  //   camera.onloadedmetadata = function(e) {
-  //     console.log("playing")
-  //     camera.play();
-  //   };
-
-  // }).catch(function() {
-  //   console.log("could not connect stream");
-  // });
+  constructor(props) {
+    super(props);
+    this.cameraRef = React.createRef();
+    this.outputRef = React.createRef();
+  }
 
   startCamera = () => {
 
     console.log("seofijsoefijseofijseif")
 
-      navigator.mediaDevices.getUserMedia({video: true}).then(function(stream) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
 
-        console.log(stream)
+      console.log(stream)
 
-        const camera = document.getElementById("camera");
-        console.log(camera)
+      const camera = document.getElementById("camera");
+      console.log(camera)
 
-        camera.srcObject = stream;
+      camera.srcObject = stream;
 
-        // camera.onloadedmetadata = function(e) {
-        //   console.log("playing")
-        //   camera.play();
-        // };
+      // camera.onloadedmetadata = function(e) {
+      //   console.log("playing")
+      //   camera.play();
+      // };
 
-    }).catch(function() {
+    }).catch(function () {
       console.log("could not connect stream");
     });
   }
 
   startOutput = () => {
 
-    const output = document.getElementById("output");
-    const outputContext = output.getContext("2d");
-    
-    const camera = document.getElementById("camera");
+    console.log(this.outputRef)
 
-    outputContext.drawImage(camera, 0, 0);
+    // const output = document.getElementById("output");
+    const outputContext = this.outputRef.current.getContext("2d");
 
+    // const camera = document.getElementById("camera");
+    console.log(outputContext)
 
-    // outputContext.drawImage(inputCanvasElement,0,0);
-  
+    outputContext.drawImage(this.cameraRef.current, 0, 0);
+
   }
-
 
 
 
 
   render() {
     return <div>
+
+      <video id="camera" autoPlay ref={this.cameraRef}></video>
+      <canvas id="output" ref={this.outputRef}></canvas>
+
       <button onClick={this.startCamera}>camera</button>
       <button onClick={this.startOutput}>output</button>
+
     </div>
   }
 
