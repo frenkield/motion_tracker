eed4f68197763a053eb6c88bd7682e5f80c492b3
frenkield <david.frenkiel@yahoo.com>
2022-03-21 11:33:41 -1000

    Some minor modifications to FaceMonitor

--- src/FaceMonitor.js
+++ src/FaceMonitor.js
@@ -1,6 +1,7 @@
 import React from "react"
-
-import { FaceMesh, VERSION, FACEMESH_RIGHT_EYE, FACEMESH_LEFT_EYE } from "@mediapipe/face_mesh"
+import {
+  FaceMesh, VERSION, FACEMESH_FACE_OVAL, FACEMESH_RIGHT_EYE, FACEMESH_LEFT_EYE
+} from "@mediapipe/face_mesh"
 import { Camera } from "@mediapipe/camera_utils"
 import { drawConnectors } from "@mediapipe/drawing_utils"
 
@@ -15,18 +16,16 @@ export default class FaceMonitor extends React.Component {
 
     this.initializeFaceMesh()
     this.running = false
+
+    console.log("FACEMESH_LEFT_EYE", FACEMESH_LEFT_EYE)
+
   }
 
   initializeFaceMesh() {
 
-    const faceMeshVersion = VERSION
-
     const config = {
       locateFile: (file) => {
-
-        const filename = `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@` +
-          `${faceMeshVersion}/${file}`;
-        return filename;
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh@${VERSION}/${file}`;
       }
     }
 
@@ -39,18 +38,56 @@ export default class FaceMonitor extends React.Component {
     this.canvasElement = this.canvasRef.current
     this.canvasCtx = this.canvasElement.getContext('2d')
 
-    const camera = new Camera(this.cameraRef.current, {
+    this.camera = new Camera(this.cameraRef.current, {
       onFrame: async () => {
         await this.faceMesh.send({ image: this.cameraRef.current });
-      },
-      width: 1280,
-      height: 720,
+      }
     });
 
-    camera.start();
+    this.camera.start();
+  }
+
+  stop = () => {
+    this.camera.stop()
   }
 
   onResults = (results) => {
+
+    this.logPerformance(results)
+
+    this.canvasCtx.save();
+    this.canvasCtx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
+
+    // this.canvasCtx.drawImage(results.image, 0, 0, this.canvasElement.width,
+    //                          this.canvasElement.height);
+
+    if (results.multiFaceLandmarks) {
+
+      for (const landmarks of results.multiFaceLandmarks) {
+
+        // if (this.resultsCount == 1) {
+        //   const faceRig = Face.solve(landmarks, {runtime:'mediapipe', video:this.cameraRef})
+        //   console.log(faceRig)
+        // }
+
+        drawConnectors(this.canvasCtx, landmarks, FACEMESH_LEFT_EYE, {color: "#FF3030"})
+        drawConnectors(this.canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: "#30FF30"})
+
+        // drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION, { color: '#C0C0C070', lineWidth: 1 })
+        // drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYE, { color: '#FF3030' })
+        // drawConnectors(canvasCtx, landmarks, FACEMESH_RIGHT_EYEBROW, { color: '#FF3030' })
+        // drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYE, { color: '#30FF30' })
+        // drawConnectors(canvasCtx, landmarks, FACEMESH_LEFT_EYEBROW, { color: '#30FF30' })
+        drawConnectors(this.canvasCtx, landmarks, FACEMESH_FACE_OVAL, { color: '#E0E0E0' })
+        // drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, { color: '#E0E0E0' })
+    
+      }
+    }
+
+    this.canvasCtx.restore();
+  }
+
+  logPerformance(results) {
 
     if (!this.running) {
       this.running = true
@@ -66,23 +103,9 @@ export default class FaceMonitor extends React.Component {
       console.log(this.resultsCount / elapsedTime * 1000)
       this.startTime = currentTime
       this.resultsCount = 0
+
+      console.log(results)   
     }
-
-    this.canvasCtx.save();
-    this.canvasCtx.clearRect(0, 0, this.canvasElement.width, this.canvasElement.height);
-
-    // this.canvasCtx.drawImage(results.image, 0, 0, this.canvasElement.width,
-    //                          this.canvasElement.height);
-
-    if (results.multiFaceLandmarks) {
-
-      for (const landmarks of results.multiFaceLandmarks) {
-        drawConnectors(this.canvasCtx, landmarks, FACEMESH_LEFT_EYE, {color: "#FF3030"});
-        drawConnectors(this.canvasCtx, landmarks, FACEMESH_RIGHT_EYE, {color: "#30FF30"});
-      }
-    }
-
-    this.canvasCtx.restore();
   }
 
   render() {
@@ -93,6 +116,7 @@ export default class FaceMonitor extends React.Component {
 
       <div>
         <button onClick={this.start}>start</button>
+        <button onClick={this.stop}>stop</button>
       </div>
 
     </div>
