716e405ecf17671fefa3c3e9341cfd16273fdb3c
frenkield <david.frenkiel@yahoo.com>
2022-03-23 09:57:31 -1000

    Refactored rig a bit

--- src/Rig.js
+++ src/Rig.js
@@ -17,16 +17,24 @@ export default class Rig extends React.Component {
 
     this.rigContainer = this.rigContainerRef.current
 
-    const renderer = new THREE.WebGLRenderer()
-    renderer.setSize(400, 300)
-    this.rigContainer.appendChild(renderer.domElement)
+    this.renderer = new THREE.WebGLRenderer()
+    this.renderer.setSize(400, 300)
+    this.rigContainer.appendChild(this.renderer.domElement)
 
-    const camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 500)
-    camera.position.set(0, 0, 100)
-    camera.lookAt(0, 0, 0)
-    camera.position.z = 5;
+    this.camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, 500)
+    this.camera.position.set(0, 0, 100)
+    this.camera.lookAt(0, 0, 0)
+    this.camera.position.z = 5;
 
-    const scene = new THREE.Scene()
+    this.scene = new THREE.Scene()
+    this.addObjectsToScene()
+    this.addLightsToScene()
+
+    this.running = true
+    this.animate()
+  }
+
+  addObjectsToScene() {
 
     const material = new THREE.MeshPhongMaterial({
       ambient: 0x555555,
@@ -37,33 +45,31 @@ export default class Rig extends React.Component {
     })
       
     this.cube = new THREE.Mesh(new THREE.BoxGeometry(), material)
-    scene.add(this.cube)
+    this.scene.add(this.cube)
+  }
 
-    scene.add( new THREE.AmbientLight(0xff0040) );
+  addLightsToScene() {
+    this.scene.add(new THREE.AmbientLight(0xff0040));
     const light = new THREE.PointLight(0xffffff, 6, 40);
     light.position.set(20, 20, 20);
-    scene.add(light);
+    this.scene.add(light);
+  }
 
-    const cube = this.cube
+  animate = () => {
+
+    requestAnimationFrame(this.animate);
+
     const headPosition = this.props.headPosition
 
-    function animate() {
+    this.cube.position.x = headPosition.x * 5 - 2.5
+    this.cube.position.y = -headPosition.y * 5 + 2.5
+    this.cube.position.z = Math.min(headPosition.z * 100 - 2, 3)
 
-      requestAnimationFrame(animate);
+    this.cube.rotation.y = headPosition.longitude
+    this.cube.rotation.x = headPosition.latitude
 
-      cube.position.x = headPosition.x * 5 - 2.5
-      cube.position.y = -headPosition.y * 5 + 2.5
-      cube.position.z = Math.min(headPosition.z * 100 - 2, 3)
-
-      cube.rotation.y = headPosition.longitude
-      cube.rotation.x = headPosition.latitude
-
-      renderer.render(scene, camera)
-    };
-
-    this.running = true
-    animate()
-  }
+    this.renderer.render(this.scene, this.camera)
+  };
 
   render() {
     return <div id="rig_container" ref={this.rigContainerRef}></div>
